c751f1f1b598e8108310b9249148d2cf7975a745
gonzaloF <floresgonzalomartin@gmail.com>
2023-03-24 14:54:50 -0300

    agrego tabla con películas

---
+++ javascript/index.js
@@ -0,0 +1,58 @@
+let $formCreate = document.querySelector("#createForm");
+let $codigoPelicula = document.querySelector("#codigoPelicula");
+let $nombrePelicula = document.querySelector("#nombrePelicula");
+let $categoriaPelicula = document.querySelector("#categoriaPelicula");
+let $descripcionPelicula = document.querySelector("#descripcionPelicula");
+let $isCheked = document.querySelector("#isCheked");
+let $contenedorPelicula = document.querySelector("#contenedorPelicula")
+let $agregarPelicula = document.querySelector("#agregarPelicula")
+
+var peliculas = []
+const peliculasEnLocalStorage = localStorage.getItem("peliculas")
+const peliculasConvertidasJS = JSON.parse(peliculasEnLocalStorage) 
+
+if(peliculasConvertidasJS !== null){ 
+  
+    peliculas = peliculasConvertidasJS
+  }
+
+$agregarPelicula.addEventListener("click",function (event){
+    event.preventDefault()
+    const peliculaNueva = {
+        codigo: $codigoPelicula.value,
+      nombre: $nombrePelicula.value,
+      categoria: $categoriaPelicula.value,
+      descripcion: $descripcionPelicula.value,
+      checked: $isCheked.checked
+    };
+    peliculas.push(peliculaNueva);
+    console.log(peliculaNueva)
+    guardarPeliculas(peliculas)
+    pintarPeliculas()
+    console.log(peliculas)
+    
+    $formCreate.reset()
+
+    
+}
+)
+
+
+const guardarPeliculas = (peliculasGuardada) => {
+    const peliculasEnJSON = JSON.stringify(peliculasGuardada);
+    const peliculasEnLocalStorage = localStorage.setItem("peliculas", peliculasEnJSON);
+  };
+
+  const pintarPeliculas = () => {
+    $contenedorPelicula.innerHTML = "";
+    peliculas.forEach((pelicula) => {
+      const estructuraPelicula = `<tr ><th scope="row">${pelicula.codigo}</th>
+      <td>${pelicula.nombre}</td>
+      <td>${pelicula.categoria}</td>
+      <td>${pelicula.descripcion}</td>
+      <td>${pelicula.checked ? "✔️" : "✖️"}</td>
+      <td>@mdo</td></tr>`;
+      $contenedorPelicula.innerHTML += estructuraPelicula;
+    });
+  };
+pintarPeliculas()
