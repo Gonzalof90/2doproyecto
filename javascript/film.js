01cfdfc3dd38feea4755b03df1d22529f132a6ec
Ezequiel Corbalán <ezequielcorbalan@gmail.com>
2023-03-25 16:50:31 -0300

    film con js

---
+++ javascript/film.js
@@ -0,0 +1,13 @@
+let $filmContainer = document.getElementById("filmContainer")
+let $bannerPelicula = document.getElementById("bannerPelicula")
+let $añoPelicula = document.getElementById("añoPelicula")
+let $duracionPelicula = document.getElementById("duracionPelicula")
+let $generoPelicula = document.getElementById("generoPelicula")
+let $verTrailer = document.getElementById("verTrailer")
+let $contenedorTrailer =document.getElementById("contenedorTrailer")
+
+
+
+$verTrailer.addEventListener("click" , () => {
+    $contenedorTrailer.innerHTML = `<iframe class=" w-100 h-100" src="https://www.youtube.com/embed/yNXfOOL8824" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+})
