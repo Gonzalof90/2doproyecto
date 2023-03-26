2337e6b0d2ab3e1ded0c88dba7fc7bf8f8fe0e7d
Ezequiel Corbalán <ezequielcorbalan@gmail.com>
2023-03-25 22:26:14 -0300

    con clase de apoyo

--- javascript/film.js
+++ javascript/film.js
@@ -6,8 +6,15 @@ let $generoPelicula = document.getElementById("generoPelicula")
 let $verTrailer = document.getElementById("verTrailer")
 let $contenedorTrailer =document.getElementById("contenedorTrailer")
 
+const idMovie = new URLSearchParams(window.location.search).get("idMovie")
+const infoLocalStorage = JSON.parse(localStorage.getItem("peliculas"))
+const movies = infoLocalStorage ? infoLocalStorage : []
+
+const movieFound = movies.find(movie => movie.codigo === idMovie)
+
+console.log(infoLocalStorage)
 
 
 $verTrailer.addEventListener("click" , () => {
-    $contenedorTrailer.innerHTML = `<iframe class=" w-100 h-100" src="https://www.youtube.com/embed/yNXfOOL8824" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+    $contenedorTrailer.innerHTML = `<iframe class=" " src="https://www.youtube.com/embed/yNXfOOL8824" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
 })
