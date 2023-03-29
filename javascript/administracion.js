92b69f751bfd7b7ad07a741b45b43186f44047fd
gonzaloF <floresgonzalomartin@gmail.com>
2023-03-29 02:55:02 -0300

    agrego columnas a tabla de administraciòn y botones

--- javascript/administracion.js
+++ javascript/administracion.js
@@ -7,7 +7,11 @@ let $isCheked = document.querySelector("#isCheked");
 let $contenedorPelicula = document.querySelector("#contenedorPelicula");
 let $agregarPelicula = document.querySelector("#agregarPelicula");
 let $opcionesDePeliculas = document.querySelector(".opcionesDePeliculas");
+let $addMovie = document.querySelector("#addMovie");
+let $tipo1 = document.querySelector("#tipo1");
+let $tipo2 = document.querySelector("#tipo2");
 
+let isCreate = true;
 let peliculas = [];
 const peliculasEnLocalStorage = localStorage.getItem("peliculas");
 const peliculasConvertidasJS = JSON.parse(peliculasEnLocalStorage);
@@ -16,22 +20,69 @@ if (peliculasConvertidasJS !== null) {
   peliculas = peliculasConvertidasJS;
 }
 
+$addMovie.addEventListener("click", function(){
+  $agregarPelicula.innerHTML = "Agregar Contenido"
+  $agregarPelicula.style.backgroundColor = "#0275d8"
+})
+
+$tipo1.addEventListener("change", function(){
+  if(isCreate){
+    if($tipo1.checked){
+    $agregarPelicula.innerHTML = "Agregar Pelìcula"
+    }
+  }
+})
+$tipo2.addEventListener("change", function(){
+  if(isCreate){
+    if($tipo2.checked){
+    $agregarPelicula.innerHTML = "Agregar Serie"
+    }
+  }
+})
+
+
+ function tipos() {
+  let radios = document.getElementsByName("tipo");
+  let selected = Array.from(radios).find(radio => radio.checked);
+  return (selected.value);
+}
+
 $createForm.addEventListener("submit", function (event) {
   event.preventDefault();
-  const peliculaNueva = {
-    codigo: generateId(),
-    nombre: $nombrePelicula.value,
-    genero: $generoPelicula.value,
-    descripcion: $descripcionPelicula.value,
-    checked: $isCheked.checked,
-  };
-  peliculas.push(peliculaNueva);
-  console.log(peliculaNueva);
-  guardarPeliculas(peliculas);
-  pintarPeliculas();
-  console.log(peliculas);
 
-  $createForm.reset();
+  if (isCreate) {
+    
+    const peliculaNueva = {
+      codigo: generateId(),
+      tipo: tipos(),
+      nombre: $nombrePelicula.value,
+      genero: $generoPelicula.value,
+      descripcion: $descripcionPelicula.value,
+      checked: $isCheked.checked,
+    };
+    peliculas.push(peliculaNueva);
+    guardarPeliculas(peliculas);
+    pintarPeliculas(peliculas);
+    $createForm.reset();
+  } else {
+    peliculas = peliculas.map((pelis) => {
+      if (pelis.codigo === parseInt($codigoPelicula.value)) {
+        return {
+          codigo: pelis.codigo,
+          tipo: tipos(),
+          nombre: $nombrePelicula.value,
+          genero: $generoPelicula.value,
+          descripcion: $descripcionPelicula.value,
+          checked: $isCheked.checked,
+        };
+      }
+      return pelis;
+    });
+    guardarPeliculas(peliculas);
+    pintarPeliculas(peliculas);
+    $createForm.reset();
+    isCreate = true;
+  }
 });
 
 const guardarPeliculas = (peliculasGuardada) => {
@@ -42,29 +93,52 @@ const guardarPeliculas = (peliculasGuardada) => {
   );
 };
 
-const pintarPeliculas = () => {
+const pintarPeliculas = (arr) => {
   $contenedorPelicula.innerHTML = "";
-  peliculas.forEach((pelicula) => {
-    const estructuraPelicula = `<tr ><th scope="row">${pelicula.codigo}</th>
-      <td>${pelicula.nombre}</td>
-      <td>${pelicula.genero}</td>
-      <td>${pelicula.descripcion}</td>
-      <td>${pelicula.checked ? "✔️" : "✖️"}</td>
-      <td class="opcionesDePeliculas">
-      <button onclick="borrarPelicula(${pelicula.codigo})" class="btn btn-light">
+  arr.forEach((pelicula) => {
+    const estructuraPelicula = `<tr ><th scope="row" class="text-center">${pelicula.codigo}</th>
+    <td class="text-center">${pelicula.tipo}</td>
+      <td class="text-center">${pelicula.nombre}</td>
+      <td class="text-center">${pelicula.genero}</td>
+      <td class="text-center">${pelicula.descripcion}</td>
+      <td class="text-center">${pelicula.checked ? "✔️" : "✖️"}</td>
+      <td class="opcionesDePeliculas text-center">
+      <button onclick="borrarPelicula(${
+        pelicula.codigo
+      })" class="btn btn-light">
       <img src="../assets/icons/papelera-de-reciclaje.png" alt="papelera-de-reciclaje" style="width:2vw" >
       </button>
-      <img src="../assets/icons/editar.png" alt="papelera-de-reciclaje" style="width:2vw">
+      <button onclick="editarPelicula(${
+        pelicula.codigo
+      })" class="btn btn-light"    data-bs-toggle="modal"
+      data-bs-target="#staticBackdrop">
+      <img src="../assets/icons/editar.png" alt="icono_de_editar" style="width:2vw">
+      </button>
+      
       <img src="../assets/icons/mensaje-destacado.png" alt="papelera-de-reciclaje" style="width:2vw"></td></tr>`;
     $contenedorPelicula.innerHTML += estructuraPelicula;
   });
 };
-pintarPeliculas();
+pintarPeliculas(peliculas);
 
 const generateId = () => Math.floor(Math.random() * 9999999);
 
-function borrarPelicula(id){
-  peliculas = peliculas.filter(pelicula=> pelicula.codigo !== parseInt(id))
-  guardarPeliculas(peliculas)
-  pintarPeliculas()
+function borrarPelicula(id) {
+  peliculas = peliculas.filter((pelicula) => pelicula.codigo !== parseInt(id));
+  guardarPeliculas(peliculas);
+  pintarPeliculas(peliculas);
+}
+
+function editarPelicula(id) {
+  isCreate = false;
+  $agregarPelicula.innerHTML = "Editar Contenido"
+  $agregarPelicula.style.backgroundColor = "green"
+  const peliculaFound = peliculas.find((peli) => peli.codigo === parseInt(id));
+  $codigoPelicula.value = peliculaFound.codigo;
+  $nombrePelicula.value = peliculaFound.nombre;
+  $generoPelicula.value = peliculaFound.genero;
+  $descripcionPelicula.value = peliculaFound.descripcion;
+  $isCheked.checked = peliculaFound.checked;
+  console.log(id);
+  console.log(peliculaFound);
 }
