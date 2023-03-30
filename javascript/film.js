95b7d99ead653d38767eeb876b93b244be364fd9
Ezequiel Corbalán <ezequielcorbalan@gmail.com>
2023-03-30 18:22:36 -0300

    avances con film

--- javascript/film.js
+++ javascript/film.js
@@ -1,10 +1,12 @@
 let $filmContainer = document.getElementById("filmContainer")
+let $nombrePelicula = document.getElementById("nombrePelicula")
 let $bannerPelicula = document.getElementById("bannerPelicula")
 let $añoPelicula = document.getElementById("añoPelicula")
 let $duracionPelicula = document.getElementById("duracionPelicula")
 let $generoPelicula = document.getElementById("generoPelicula")
 let $verTrailer = document.getElementById("verTrailer")
 let $contenedorTrailer =document.getElementById("contenedorTrailer")
+let $sinopsisPelicula = document.getElementById("sinopsisPelicula")
 
 const idMovie = new URLSearchParams(window.location.search).get("idMovie")
 const infoLocalStorage = JSON.parse(localStorage.getItem("peliculas"))
@@ -12,9 +14,17 @@ const movies = infoLocalStorage ? infoLocalStorage : []
 
 const movieFound = movies.find(movie => movie.codigo === idMovie)
 
-console.log(infoLocalStorage)
+console.log(movies)
+$nombrePelicula.textContent = movieFound.nombre
+$sinopsisPelicula.textContent = movieFound.descripcion 
+$bannerPelicula.textContent = movieFound.
+$añoPelicula.textContent = movieFound.
+$generoPelicula.textContent = movieFound.
+$duracionPelicula.textContent = movieFound.
+$contenedorTrailer.textContent = movieFound.
 
 
 $verTrailer.addEventListener("click" , () => {
     $contenedorTrailer.innerHTML = `<iframe class=" " src="https://www.youtube.com/embed/yNXfOOL8824" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
 })
+
