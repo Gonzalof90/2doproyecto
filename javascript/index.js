e680900f29fc4b81652987af8f66617c4dd16d51
Juan-Holgado <juanseholgado@gmail.com>
2023-04-09 12:26:20 -0300

    arreglamos el pintado del destacado

--- javascript/index.js
+++ javascript/index.js
@@ -1,40 +1,37 @@
-let destacadaEnLocalStorage = localStorage.getItem("peliDestacada");
+/*let destacadaEnLocalStorage = localStorage.getItem("peliDestacada");
 let destacadaConvertidasJS = JSON.parse(destacadaEnLocalStorage);
 let destacada = destacadaConvertidasJS
-
-
-let [pelicula] = destacada
-let {urlImageBigSize,nombre,genero,descripcion} = pelicula
-
 let $contenedorSlider01 = document.querySelector("#contenedorSlider01")
+*/
+const infoLocalStorage = JSON.parse(localStorage.getItem("movies"))
+console.log(infoLocalStorage)
+const getMoviesFilter = (arr = []) => {
+    const outstanding = arr.length
+      ? arr.filter((movie) => movie.outstanding)
+      : [];
+  
+    const noOutstanding = arr.length
+      ? arr.filter((movie) => !movie.outstanding)
+      : [];
+  
+    return { outstanding, noOutstanding };
+  };
 
+ const {outstanding,noOutstanding} = getMoviesFilter(infoLocalStorage)
+  console.log(outstanding)
+  console.log(noOutstanding)
+
+let [pelicula] = outstanding
+let {urlImageBigSize,nombre,genero,descripcion} = pelicula
 let $destacado = document.querySelector("#destacado")
 $destacado.innerHTML = `<div class="contenedor w-100">
 <img class="w-100" src="${urlImageBigSize}"/>
 <div class="centrado text-light bg-dark display-5 w-100">Titulo:${nombre}<br>Genero:${genero}<br>${descripcion}</div>
 </div>`
 
-/*<div class="w-100 bg-dark" style="position: relative;">asdasdas<img src="${urlImageBigSize}"class="w-50" alt="Pelicula destacada" draggable="false" style=" position: absolute; z-index: 2;"></div>*/
-
-/* Envio a detalles  */
-
-/*const idMovie = new URLSearchParams(window.location.search).get("idMovie")*/
-const infoLocalStorage = JSON.parse(localStorage.getItem("peliculas"))
-const movies = infoLocalStorage ? infoLocalStorage : []
+/*const movies = infoLocalStorage ? infoLocalStorage : []
 
 const urlImageSmallSize = movies.map(movie => (movie.urlImageSmallSize) )
-
-/*
-const movieFound = movies.find(movie => movie.codigo === parseInt(idMovie))*/
-/*console.log(movies)*/
-
-/*
-<li>
-<a href="http://">
-<img onclick="enviarInfoFilm()" src="${urlImageSmallSize}"
-    width="100%" alt="...">
-</a>
-</li>*/
 
 
 function pintarslider(array){
@@ -53,3 +50,5 @@ function pintarslider(array){
 
                              pintarslider(movies)
                             console.log(pelicula.urlImageSmallSize)
+
+                            */
