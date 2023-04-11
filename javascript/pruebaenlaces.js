ee8637e37c5c6497bca40fe46d091737bfb78537
gonzaloF <floresgonzalomartin@gmail.com>
2023-04-11 10:37:04 -0300

    corrijo formulario administración

--- javascript/pruebaenlaces.js
+++
@@ -1,9 +0,0 @@
-
-
-let destacadaEnLocalStorage = localStorage.getItem("peliDestacada");
-let destacadaConvertidasJS = JSON.parse(destacadaEnLocalStorage);
-let destacada = destacadaConvertidasJS
-console.log(destacada)
-let [pelicula] = destacada
-let {urlImageBigSize} = pelicula
-console.log(urlImageBigSize)
